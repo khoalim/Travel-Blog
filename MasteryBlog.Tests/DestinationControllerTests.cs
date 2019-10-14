7e0659c3f8e2f77d44780776c08435235cf66cbe
William Harris <williamharris2021@gmail.com>
2019-10-14 15:53:30 -0400

    Added DestinationController Tests

---
+++ MasteryBlog.Tests/DestinationControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+using MasteryBlog.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using MasteryBlog.Repositories;
+using MasteryBlog.Models.Destination;
+
+namespace MasteryBlog.Tests
+{
+    public class DestinationControllerTests
+    {
+        IRepository<Destination> destinationRepo;
+
+        [Fact]
+        public void Index_Returns_A_View()
+        {
+            var underTest = new DestinationController(destinationRepo);
+
+            var result = underTest.Index();
+
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
