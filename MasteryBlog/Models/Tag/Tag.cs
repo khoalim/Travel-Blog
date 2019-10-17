9c7402fb3fa9ee02a8524581e0aef725f99de94b
Kim Hoalim <kthoalim@gmail.com>
2019-10-17 14:49:12 -0400

    Refactored Links and Added Tag Model

---
+++ MasteryBlog/Models/Tag/Tag.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MasteryBlog.Models.Tag
+{
+    public class Tag
+    {
+        public int TagID { get; set; }
+        public string Name { get; set; }
+    }
+}
