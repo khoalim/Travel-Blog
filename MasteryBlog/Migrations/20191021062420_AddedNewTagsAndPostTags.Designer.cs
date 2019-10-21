1f9a9529d9343d7b6d72f37d21f97077d9923c50
Kim Hoalim <kthoalim@gmail.com>
2019-10-21 03:52:31 -0400

    Added Landing Page Style

--- MasteryBlog/Migrations/20191021062420_AddedNewTagsAndPostTags.Designer.cs
+++ MasteryBlog/Migrations/20191021062420_AddedNewTagsAndPostTags.Designer.cs
@@ -4,14 +4,16 @@ using MasteryBlog.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MasteryBlog.Migrations
 {
     [DbContext(typeof(BlogContext))]
-    partial class BlogContextModelSnapshot : ModelSnapshot
+    [Migration("20191021062420_AddedNewTagsAndPostTags")]
+    partial class AddedNewTagsAndPostTags
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
